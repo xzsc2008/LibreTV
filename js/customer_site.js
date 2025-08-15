bd71430843db05b65705059a6803ff6333b62cfc
xzsc2008 <150655287+xzsc2008@users.noreply.github.com>
2025-08-15 15:39:14 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -2,6 +2,14 @@ const CUSTOMER_SITES = {
     qiqi: {
         api: 'https://www.qiqidys.com/api.php/provide/vod',
         name: '七七资源',
+        api: 'http://caiji.dyttzyapi.com/api.php/provide/vod',
+        name: '电影天堂资源',
+        api: 'https://cj.rycjapi.com/api.php/provide/vod',
+        name: '如意资源',
+        api: 'https://bfzyapi.com/api.php/provide/vod',
+        name: '暴风资源',
+        api: 'https://json.heimuer.xyz/api.php/provide/vod',
+        name: '黑木耳资源',
     }
 };
 
