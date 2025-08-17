3b59b392deeb8ca0cc3cef7e61b00c8dfaaf6d53
xzsc2008 <150655287+xzsc2008@users.noreply.github.com>
2025-08-18 00:26:40 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -22,9 +22,9 @@ const SITE_CONFIG = {
 
 // API站点配置
 const API_SITES = {
-    testSource: {
-        api: 'https://www.example.com/api.php/provide/vod',
-        name: '空内容测试源',
+    heimuer: {
+        api: 'https://json.heimuer.xyz/api.php/provide/vod',
+        name: '黑木耳',
         adult: true
     }
     //ARCHIVE https://telegra.ph/APIs-08-12
