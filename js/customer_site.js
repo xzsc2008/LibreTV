b93906b864422e4d3467e54c81600d1348f06bb8
xzsc2008 <150655287+xzsc2008@users.noreply.github.com>
2025-08-18 00:34:41 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -3,12 +3,13 @@ const CUSTOMER_SITES = {
         api: 'http://caiji.dyttzyapi.com/api.php/provide/vod',
         name: '电影天堂资源',
     }
+};
+const CUSTOMER_SITES = {
     heimuer: {
         api: 'https://json.heimuer.xyz/api.php/provide/vod',
         name: '黑木耳',
     }
 };
-
 // 调用全局方法合并
 if (window.extendAPISites) {
     window.extendAPISites(CUSTOMER_SITES);
